c7ed6726d8649b3e995a3ca86673c2823e97051b
xfs <colfr.76@gmail.com>
2014-08-20 08:17:35 +0200

    Ajout de urls.py dans Article

---
+++ blog/Article/urls.py
@@ -0,0 +1,12 @@
+#-*- coding:utf-8 -*-
+from django.conf.urls import patterns, include, url
+
+
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'blog.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^article/', ''),
+)
